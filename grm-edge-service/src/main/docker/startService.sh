ec10324644304147664b692f368e2607f159b26c
Appampally, Sripad (sa837d) <sa837d@att.com>
2017-11-15 12:55:04 -0600

    Final touch up,changed jar version

--- grm-edge-service/src/main/docker/startService.sh
+++ grm-edge-service/src/main/docker/startService.sh
@@ -1,5 +1,4 @@
 #*******************************************************************************
 # Copyright (c) 2017 AT&T Intellectual Property. All rights reserved.
 #*******************************************************************************
-cp /grm-edge-cass-override-config/persistence-client.properties /grm-edge-cass-persistence-config/persistence-client.properties
-java -Xms1024m -Xmx1024m -DCASS_DB=etc/config/application.properties -jar /app.jar -DGRID_CONFIG_FILE=/grm-edge-cass-persistence-config -DGRID_CONFIG_FILE_OVERRIDE=persistence-client.properties
+java -Xms1024m -Xmx1024m -jar /app.jar -DGRID_CONFIG_FILE=/grm-edge-cass-persistence-config 
